99a2e275ee01c096e31e83cbd3b707fb35c0f4da
medkrimi <med.krimi@gmail.com>
2022-11-04 10:43:07 -0400

    Configured husky to run pre-commit hook.

--- src/lib/utils/formatDate.ts
+++ src/lib/utils/formatDate.ts
@@ -7,7 +7,7 @@ import {format} from "date-fns"
  *
  * https://date-fns.org/v2.29.2/docs/format
  */
-export function f ormatDate(isoDateString: string) {
+export function formatDate(isoDateString: string) {
   const date = new Date(isoDateString)
   const formattedDate = format(date, "MMMM do 'at' h:mmaaa")
   return formattedDate
