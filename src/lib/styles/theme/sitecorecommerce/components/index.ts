99b615eb06d4a024b9a81d66b94a3324de014bd4
George Haeger <geh@sitecore.net>
2022-11-07 14:14:15 -0600

    Updates to remove unused variables files

--- src/lib/styles/theme/sitecorecommerce/components/index.ts
+++ src/lib/styles/theme/sitecorecommerce/components/index.ts
@@ -1,9 +1,11 @@
-import type {DeepPartial, Theme} from "@chakra-ui/react"
-
 import {Button} from "./button"
 import {Card} from "./card"
 
-export const components: DeepPartial<Theme["components"]> = {
-  Button,
-  Card
-}
+import {extendTheme} from "@chakra-ui/react"
+
+export const components = extendTheme({
+  components: {
+    Button,
+    Card
+  }
+})
