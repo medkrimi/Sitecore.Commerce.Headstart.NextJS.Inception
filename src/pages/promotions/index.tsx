1a36385357c603cd8aac24aa98428e1f2a392eac
George Haeger <geh@sitecore.net>
2023-01-24 17:01:29 -0600

    added dialog for download

--- src/pages/promotions/index.tsx
+++ src/pages/promotions/index.tsx
@@ -14,7 +14,8 @@ import {
   Stack,
   Text,
   useToast,
-  AlertDialogFooter
+  AlertDialogFooter,
+  Spinner
 } from "@chakra-ui/react"
 import {useEffect, useRef, useState} from "react"
 
@@ -185,46 +186,16 @@ const PromotionsList = () => {
   return (
     <>
       <PromotionsDataTable tableData={promotions} columnsData={columnsData} />
-      <AlertDialog
-        isOpen={isExportCSVDialogOpen}
-        onClose={() => setExportCSVDialogOpen(false)}
-        leastDestructiveRef={cancelRef}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              Export Selected Promotions to CSV
-            </AlertDialogHeader>
-            <AlertDialogBody>
-              <Text display="inline">
-                Export the selected promotions to a CSV, once the export button is clicked behind the scenes a job will
-                be kicked off to create the csv and then will automatically download to your downloads folder in the
-                browser.
-              </Text>
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <HStack justifyContent="space-between" w="100%">
-                <Button
-                  ref={cancelRef}
-                  onClick={() => setExportCSVDialogOpen(false)}
-                  disabled={loading}
-                  variant="secondaryButton"
-                >
-                  Cancel
-                </Button>
-                <Button onClick={requestExportCSV} disabled={loading}>
-                  {loading ? <Spinner color="brand.500" /> : "Export Promotions"}
-                </Button>
-              </HStack>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
     </>
   )
 }
 
 const ProtectedBuyersList = () => {
+  const [isExportCSVDialogOpen, setExportCSVDialogOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const cancelRef = useRef()
+  const requestExportCSV = () => {}
+
   return (
     <ProtectedContent hasAccess={appPermissions.OrderManager}>
       <Box padding="GlobalPadding">
@@ -242,6 +213,42 @@ const ProtectedBuyersList = () => {
         <Card variant="primaryCard">
           <PromotionsList />
         </Card>
+
+        <AlertDialog
+          isOpen={isExportCSVDialogOpen}
+          onClose={() => setExportCSVDialogOpen(false)}
+          leastDestructiveRef={cancelRef}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Export Selected Promotions to CSV
+              </AlertDialogHeader>
+              <AlertDialogBody>
+                <Text display="inline">
+                  Export the selected promotions to a CSV, once the export button is clicked behind the scenes a job
+                  will be kicked off to create the csv and then will automatically download to your downloads folder in
+                  the browser.
+                </Text>
+              </AlertDialogBody>
+              <AlertDialogFooter>
+                <HStack justifyContent="space-between" w="100%">
+                  <Button
+                    ref={cancelRef}
+                    onClick={() => setExportCSVDialogOpen(false)}
+                    disabled={loading}
+                    variant="secondaryButton"
+                  >
+                    Cancel
+                  </Button>
+                  <Button onClick={requestExportCSV} disabled={loading}>
+                    {loading ? <Spinner color="brand.500" /> : "Export Promotions"}
+                  </Button>
+                </HStack>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
       </Box>
     </ProtectedContent>
   )
